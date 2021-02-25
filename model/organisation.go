81de7582b61f3cb7a8406b7278b8052667a128f1
Jasmina Pandit <jasminapandit@192-168-1-103.tpgi.com.au>
2021-02-25 12:22:41 +1100

    Create models for ref data

---
+++ model/organisation.go
@@ -0,0 +1,13 @@
+package model
+
+type Organisation struct {
+	ID            int      `json:"_id"`
+	URL           string   `json:"url"`
+	ExternalID    string   `json:"external_id"`
+	Name          string   `json:"name"`
+	DomainNames   []string `json:"domain_names"`
+	CreatedAt     string   `json:"created_at"`
+	Details       string   `json:"details"`
+	SharedTickets bool     `json:"shared_tickets"`
+	Tags          []string `json:"tags"`
+}
